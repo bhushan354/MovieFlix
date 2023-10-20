06a333a081337f5a46fbc92975d3cc5200f89ca2
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-20 07:12:50 +0530

    Configure store using redux Doc.& add data inside url in homeSlice.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable */
 import { useState, useEffect } from 'react';
 import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -10,6 +14,7 @@ function App() {
   const apiTesting = async () => {
       fetchDataFromApi("/movie/popular").then((res)=>{
         console.log(res);
+        dispatch(getApiConfiguration(res));
       });
 };
 
