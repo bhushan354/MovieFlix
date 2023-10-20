e69310c256c8789cb2bc7c6ecf541918a47fc944
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-20 07:37:51 +0530

    Done with store

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,30 @@ import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
   const dispatch = useDispatch();
+  //to use values from url
+  // {} this is used to destructure data from url
+  //this state contains whole store means url and genres ,use what you want
+  const { url } = useSelector((state) =>
+    //this state is returning reducer object from store.js
+    state.home);
+    // this state.home is stored to url object with destructuring
+    console.log(url);
 
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = async () => {
-      fetchDataFromApi("/movie/popular").then((res)=>{
-        console.log(res);
-        dispatch(getApiConfiguration(res));
-      });
-};
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
 
   return (
     <div className="App">
       App
+      {url?.total_pages}
     </div>
   )
 }
